62cf79a67e6bf40bd746eef97126449133723922
Goutham Pilla <goutham2027@gmail.com>
2016-02-22 14:07:08 +0530

    Initial Commit

---
+++ ex-4.py
@@ -0,0 +1,5 @@
+# Ex-4
+# Write a method which takes name, age as arguments.
+# Output should be: My name is <name> and my age is <age>
+# eg: person_details('Abc', 22)
+# Output: My name is Abc and my age is 22.
---
+++ ex-9.py
@@ -0,0 +1,2 @@
+# Ex-9
+# Import math module and print the pi value
